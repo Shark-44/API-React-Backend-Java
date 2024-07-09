6d9c17270183290f404a0e613738cc190ffdeaa2
Shark-44 <joanny.bernardeau@gmail.com>
2024-07-09 09:30:10 +0200

    sauvegarde apres modifs du backend pour upload de nouvelles images

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,17 +1,34 @@
 import "./Card.css"
 import { useEffect, useState } from "react";
 import axios from "axios";
+import {Popover, PopoverTrigger, PopoverContent, Button} from "@nextui-org/react";
 
 type Student = {
     id: number;
     name: string;
     firstname: string;
     photo: string;
+    birthday:string;
+    langages: { idLangage: number; nameLangage: string }[];
     
 }
 
-function Card () {
+interface CardProps {
+    onPopoverOpen: () => void;
+    onPopoverClose: () => void;
+}
+
+const formatDate = (dateString: string) => {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${day}-${month}-${year}`;
+}
+
+function Card ({ onPopoverOpen, onPopoverClose }: CardProps) {
     const [listStudent, setListStudent] = useState<Student[]>([]);
+    
 
     useEffect (() => {
         axios.get('http://localhost:8080/api/students')
@@ -20,19 +37,38 @@ function Card () {
                 console.error('Error fetching data:', error);
             });
     },[])
+  
     console.log(listStudent)
     return (
-        <div>
+        <div className="carddetail">
             {listStudent.map((list, index) => {
                 return (
-                    <div key={index} className="details">
-                        <img src={list.photo} alt="" />
-                        <p> Nom : {list.name}</p>
-                        <p>Prenom : {list.firstname}</p>
-
+                    <div key={index} className="globalCard">
+                        <div className="details">
+                            <img src={`http://localhost:8080/uploads/images/${list.photo}`} alt="" />
+                            <p> Nom : {list.name}</p>
+                            <p>Prenom : {list.firstname}</p>
+                        </div>
+                        <Popover 
+                            placement="top" 
+                            showArrow={true}
+                            onOpenChange={(open) => open ? onPopoverOpen() : onPopoverClose()}
+                        >
+                        <PopoverTrigger>
+                          <Button color="primary" variant="flat" className="capitalize">Click info</Button>
+                        </PopoverTrigger>
+                        <PopoverContent>
+                            <div className="px-1 py-2">
+                                <div className="text-small font-bold">Anniversaire : {formatDate(list.birthday)}</div>
+                                <p>Langages étudiées : {list.langages.map(langage => langage.nameLangage).join(', ')}</p>
+                            </div>
+                        </PopoverContent>
+                      </Popover>
                     </div>
+
                 )
             })}
+         
         </div>
     )
 }
--- src/pages/Student.tsx
+++ src/pages/Student.tsx
@@ -1,10 +1,24 @@
-import "./Student.css"
-import Cards from "../components/Card"
+import "./Student.css";
+import Card from "../components/Card";
+import { useState } from "react";
 
-function Student () {
-    return <div className="contenairStudient">
-        <Cards />
-    </div>;
+function Student() {
+    const [isPopoverOpen, setIsPopoverOpen] = useState<boolean>(false);
+
+    const handlePopoverOpen = () => {
+        setIsPopoverOpen(true);
+    };
+
+    const handlePopoverClose = () => {
+        setIsPopoverOpen(false);
+    };
+
+    return (
+        <div className={`contenairStudient ${isPopoverOpen ? 'blurred' : ''}`}>
+            <h1>Liste des étudiants</h1>
+            <Card onPopoverOpen={handlePopoverOpen} onPopoverClose={handlePopoverClose} />
+        </div>
+    );
 }
 
 export default Student;
